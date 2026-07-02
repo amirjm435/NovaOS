192b7ea71cfa7258cc304c1293ba8d3fd0e8f75e
amirjm435 <amirjm4352gmail.com>
2026-07-02 11:48:26 +0330

    Refactor project structure

--- js/boot.js
+++ js/boot.js
@@ -1,1 +1,19 @@
 // NovaOS Boot System
+const loadingText = document.querySelector("#boot-screen p");
+
+let dots = 0;
+
+setInterval(() => {
+
+    dots++;
+
+    if (dots > 3) dots = 1;
+
+    loadingText.textContent = "Initializing" + ".".repeat(dots);
+
+}, 500);
+setTimeout(() => {
+
+    loadingText.textContent = "Loading Desktop...";
+
+}, 3000);
